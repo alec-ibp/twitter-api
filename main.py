d5ce6202ce4f60a0c69ab1a1f15ba8544ec863cf
Alejandro <alejo1ibarra@gmail.com>
2021-11-01 15:21:25 -0500

    models_users

---
+++ main.py
@@ -0,0 +1,55 @@
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+#fastAPI
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+#Models
+class UserBase(BaseModel):
+    # unique identifier 
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
+
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=2,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=2,
+        max_length=50
+    )
+    birthday: Optional[date] = Field(default=None)
+
+
+class Tweet(BaseModel):
+    pass
+
+
+@app.get(
+    path='/',
+    tags=['Home'])
+def home():
+    return {
+        'Twitter API': 'Working!'
+    }
